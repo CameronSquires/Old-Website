3b9e6036da79d232730128bcc110e835b76daf72
Cameron Squires <cameronsquires34@gmail.com>
2021-08-29 17:00:59 -0230

    Slider and icons
    Added a slickslider to show my projects. Also added a mailto button in bottom right

---
+++ slick.js
@@ -0,0 +1,11 @@
+            
+$(document).ready(() => {
+    $('.mainslider').slick({
+    arrows:false,
+    dots:false,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    });
+});
