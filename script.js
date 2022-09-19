6cf881e8b6c50e84c38bea6d62c5358b8db764bc
CameronSquires <87430045+CameronSquires@users.noreply.github.com>
2022-09-19 18:19:41 -0230

    Fixed collapsible tab in nav.

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 function togglenav() {
-    if (document.getElementById("hiddentabs").style.display === "none") {
+    if (document.getElementById("hiddentabs").style.display !== "block") {
         document.getElementById("hiddentabs").style.display = "block";
-        document.getElementById("icon").classList.toggle("rotate");
+
     }
     else {
         
         document.getElementById("hiddentabs").style.display = "none";
     }
+    document.getElementById("icon").classList.toggle("rotate");
 }
 
-//function altnavretract() {
-//    if (document.getElementById("hiddentabs").style.display === "block") {
-  //      document.getElementById("hiddentabs").style.display = "none";
-    //}
-//}
+function altnavretract() {
+    if (document.getElementById("hiddentabs").style.display === "block") {
+        document.getElementById("hiddentabs").style.display = "none";
+        document.getElementById("icon").classList.toggle("rotate");
+    }
+}
 
 
